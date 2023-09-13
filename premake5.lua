2225645f6da098cb693376ddfaabb3d3be605adc
ShikharBind <shikhar.bind2010@gmail.com>
2023-09-14 04:02:07 +0530

    updated premake for static

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,8 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-    staticruntime "off"
+	cppdialect "C++17"
+    staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -23,7 +24,6 @@ project "ImGui"
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
 
 	filter "system:linux"
 		pic "On"
@@ -38,7 +38,7 @@ project "ImGui"
 		runtime "Release"
 		optimize "on"
 
-    filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
+	-- filter "configurations:Dist"
+	-- 	runtime "Release"
+	-- 	optimize "on"
+	-- 	symbols "off"
